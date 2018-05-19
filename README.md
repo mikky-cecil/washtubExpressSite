7147d605c8aa875f748d8e6ca1e07ce78ad6b3ad
mikky-cecil <mikkycecil@gmail.com>
2018-05-18 17:24:21 -0700

    Added README, UI is now functional again (yay!)

---
+++ README.md
@@ -0,0 +1,14 @@
+# Washtub Laundromat Machine Inventory
+
+A web application for tracking machine inventory for the Washtub Laundromat. Made with Node.js, Express, Javascript, jQuery, and Bootstrap. Demonstrates basic REST functionality.
+
+### Prerequirements
+* [Node.js](https://nodejs.org/en/download/)
+* [Mongodb](https://www.mongodb.org/downloads#production)
+
+### Preparation
+* Clone repository
+* Create folder ./data in repository root folder
+* run ``` npm install ``` in the working directory
+* run ``` mongod --dbpath ./data ``` to start up the mongodb
+* run ``` npm start ``` and browse to http://localhost:3000
--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,23 +1,146 @@
-// function fillTable(){
+function fillTable(){
+	// un-select any machine info being shown
+	$('#alertMachineInfoEmpty').show();
+	$('#machineInfo p').hide();
 
-// }
+	$('#machineList tbody tr').remove(); // clear table first
 
-// function showMachineInfo(event){
+	$.ajax({
+		url: '/machines/machinelist',
+		type: 'GET',
+		dataType: 'json'
+	}).done(function(data){
+		if (data.length == 0){
+			$('#alertEmptyMachineList').show();
+			$('#machineList table').hide();
+		}else{
+			$('#alertEmptyMachineList').hide();
+			$('#machineList table').show();
+		}
 
-// }
+		var rows = [];
+		for (var i = 0; i < data.length; i++){
+			var row = $('<tr>');
+			var cols = [];
 
-// function addMachine(){
+			var machine = data[i];
+			cols.push($('<td>').html('<a href="" class="linkShowMachine" rel=' + machine._id + '>' + machine.type + '</a>'));
+			cols.push($('<td>').text(machine.load + 'lbs'));
+			cols.push($('<td>').html('<a href="" class="linkDeleteMachine text-danger" rel=' + machine._id + '>Delete</a>'));
+			
+			row.append(cols);
+			rows.push(row);
+		}
 
-// }
+		$('#machineList table').append(rows);
+	}).fail(function(){
+		console.log('Error loading machine list');
+	});
 
-// function deleteMachine(){
+}
 
-// }
+function showMachineInfo(event){
+	event.preventDefault();
+	$('#alertMachineInfoError').hide();
+	$('#alertMachineInfoEmpty').hide();
+	$('#machineInfo p').show();
 
-// $(document).ready(function(){
-// 	fillTable();
+	var machineId = $(event.target).attr('rel');
 
-// 	$().on('click', , showMachineInfo);
-// 	$().on('click', , addMachine);
-// 	$().on('click', , deleteMachine);
-// });
+	$.ajax({
+		url: '/machines/machine/' + machineId,
+		type: 'GET',
+		dataType: 'json'
+	}).done(function(data){
+		console.log(data);
+
+		$('#machineInfoType').text(data.type);
+		$('#machineInfoLoad').text(data.load + 'lbs');
+		$('#machineInfoCost').text('$' + data.cost);
+	}).fail(function(){
+		$('#alertMachineInfoError').show();
+		console.log('Error loading machine');
+	});	
+}
+
+function isCurrencyValid(currency){
+	if (currency.match(/^[0-9]+\.[0-9]{2}$/)){
+		var cash = parseFloat(currency); 
+		if (cash == NaN){
+			return false;
+		}else{
+			return true;
+		}
+	}else{
+		return false;
+	}
+}
+
+function addMachine(event){
+	event.preventDefault();
+
+	var machine = {
+		type: $('#inputMachineType').val(),
+		load: $('#inputMachineLoad').val(),
+		cost: $('#inputMachineCost').val(),
+	};
+
+	$('#inputMachineCost, #inputMachineLoad').removeClass('is-invalid');
+
+	var error = false;
+	if (!isCurrencyValid(machine.cost)){
+		error = true;
+		console.log('Cost not in valid USD format');
+		$('#inputMachineCost').addClass('is-invalid');
+	}
+	if (machine.load == '' || parseInt(machine.load) == NaN){
+		error = true;
+		console.log('Load not a whole number');
+		$('#inputMachineLoad').addClass('is-invalid');
+	}
+	if (error){
+		return;
+	}
+
+	$.ajax({
+		url: '/machines/addmachine',
+		type: 'POST',
+		dataType: 'json',
+		data: machine
+	}).done(function(data){
+		$('#addMachine input').val('');
+
+		fillTable();
+	}).fail(function(){
+		console.log('Error adding machine');
+	});
+}
+
+function deleteMachine(){
+	event.preventDefault();
+	var machineId = $(event.target).attr('rel');
+
+	$.ajax({
+		url: '/machines/deletemachine/' + machineId,
+		type: 'DELETE',
+		dataType: 'json'
+	}).done(function(data){
+		console.log(data);
+
+		fillTable();
+	}).fail(function(){
+		console.log('Error deleting machine');
+	});
+}
+
+$(document).ready(function(){
+	fillTable();
+
+	$('#alertMachineInfoError').hide();
+	$('#alertEmptyMachineList').hide();
+	$('#machineInfo p').hide();
+
+	$('#machineList tbody').on('click', 'a.linkShowMachine', showMachineInfo);
+	$('#btnAddMachine').on('click', addMachine);
+	$('#machineList tbody').on('click', 'a.linkDeleteMachine', deleteMachine);
+});
